f63b1afd5c5eb12b7968f8d4cabc3176e8a973c2
noname <145252184+ww2766@users.noreply.github.com>
2023-10-23 22:08:52 +0800

    Update install_zh_fonts.sh

--- install_zh_fonts.sh
+++ install_zh_fonts.sh
@@ -4,9 +4,5 @@ unzip Open_Da1.zip
 unzip Open_Da2.zip
 unzip Open_Da3.zip 
 
-if [ -d "Open_Data/" ] ; then
-		cp -i Open_Data/Fonts/TW-Kai-*.ttf $HOME/.fonts
-	else
-		cp -i data/TW-Kai-*.ttf $HOME/.fonts
-	fi
- sudo fc-cache -fv
+cp -i Open_Data/Fonts/TW-Kai-*.ttf $HOME/.fonts
+sudo fc-cache -fv
