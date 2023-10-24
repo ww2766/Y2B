6c6fb4226cc4e4e0e7d3ba9fb6aabdc8cede337f
noname <145252184+ww2766@users.noreply.github.com>
2023-10-24 21:47:24 +0800

    Update install_zh_fonts.sh

--- install_zh_fonts.sh
+++ install_zh_fonts.sh
@@ -4,5 +4,10 @@ unzip Open_Da1.zip
 unzip Open_Da2.zip
 unzip Open_Da3.zip 
 
+# if ~/.fonts dir not exist, then create it.
+if [ ! -d "$HOME/.fonts" ] ; then
+    mkdir $HOME/.fonts
+    echo -e "create ~/.fonts/ \n"
+fi
 cp -i Open_Data/Fonts/TW-Kai-*.ttf $HOME/.fonts
 sudo fc-cache -fv
