507a0b50b9f023a7e5cd0f608897548dd8527440
noname <145252184+ww2766@users.noreply.github.com>
2023-10-27 13:52:50 +0800

    Update dy_upload.py

--- dy_upload.py
+++ dy_upload.py
@@ -156,7 +156,7 @@ def upload(playwright: Playwright,video,cover,config,detail,cookie) -> None:
     title = detail['title']
     if len(title) > 80:
         title = title[:80]
-    browser =  playwright.chromium.launch(headless=True, args=['--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36'])
+    browser =  playwright.chromium.launch(headless=True, args=['--user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.19 (KHTML, like Gecko) Ubuntu/11.10 Chromium/18.0.1025.142 Chrome/18.0.1025.142 Safari/535.19'])
     #browser =  playwright.chromium.launch(headless=False)
     context =  browser.new_context(storage_state=cookie,locale="zh-CN")
     logging.info("授权位置权限")
