047ad0e84dec2602c7811904d80e77b298442925
noname <145252184+ww2766@users.noreply.github.com>
2023-10-31 15:14:59 +0800

    Update dy_upload.py

--- dy_upload.py
+++ dy_upload.py
@@ -196,8 +196,8 @@ def upload(playwright: Playwright,video,cover,config,detail,cookie) -> None:
         try:
             page.get_by_role("button", name="确定").click()
             print("出现了《云草稿自动保存》的弹出《确定》按钮")
-            page.get_by_role("button", name="创建").click()
-            print("出现了《选择画布》的弹出《创建》按钮")
+            page.get_by_role("button", name="取消").click()
+            print("出现了《选择画布》的弹出《取消》按钮")
         except:
             print("没有找到《云草稿自动保存》《创建》的按钮")
             pass
