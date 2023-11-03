615ba00bd9c78cfb3a5b262afaa441b204eba359
noname <145252184+ww2766@users.noreply.github.com>
2023-11-03 16:41:53 +0800

    Update dy_upload.py

--- dy_upload.py
+++ dy_upload.py
@@ -448,6 +448,11 @@ def merge_subs(all_file_list):
             filecount = filecount + 1
             with open(file_name,"r",encoding="utf-8") as fileread:
                 mergesub_list = list(srt.parse(fileread,ignore_errors=False))
+                for i in range (1,len(mergesub_list)-1):
+                    if len(mergesub_list[i-1].content.split("\n"))<2:
+                        continue
+                    if mergesub_list[i].content.split("\n")[0]==mergesub_list[i-1].content.split("\n")[1]:
+                        mergesub_list[i].content=mergesub_list[i].content.split("\n")[1]
                 if filecount == 1:
                     for i in range (len(mergesub_list)):
                         mergesub.append(mergesub_list[i])
